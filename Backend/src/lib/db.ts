b32abc8f18c77fd05ce43bcbfc2cc5b663882ade
Abdul Rafay <126176270+git-rafaykhan@users.noreply.github.com>
2025-08-02 12:21:36 -0700

    Update db.ts

--- Backend/src/lib/db.ts
+++ Backend/src/lib/db.ts
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 
 export async function connectToDb(){
     try {
-  await mongoose.connect("mongodb+srv://abdulrafay:abdulrafay@projects.srwxpnd.mongodb.net/chat-appy?retryWrites=true&w=majority&appName=Projects");
+  await mongoose.connect("");
   console.log('db connected');
 } catch (error) {
   console.log(error)
 }
+
 }
