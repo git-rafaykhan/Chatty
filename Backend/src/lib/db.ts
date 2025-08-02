d3f82527ed168d0e9ea680d003d7fcb00982cd4e
git-rafaykhan <rafaykhan2k19@gmail.com>
2025-08-02 14:12:46 -0700

    update db

--- Backend/src/lib/db.ts
+++ Backend/src/lib/db.ts
@@ -7,11 +7,7 @@ const url = process.env.MONGODB_URL;
 
 export async function connectToDb(){
     try {
-<<<<<<< HEAD
   await mongoose.connect(url!);
-=======
-  await mongoose.connect("");
->>>>>>> b32abc8f18c77fd05ce43bcbfc2cc5b663882ade
   console.log('db connected');
 } catch (error) {
   console.log(error)
